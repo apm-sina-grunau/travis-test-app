eea26b232c84b9e25546e3f470a7a54c836a9a38
Sina Grunau <sina@appmotion.de>
2018-07-13 10:33:41 +0200

    chore(travis): change something to trigger a travis build (maybe? )

--- app/src/main/java/de/sgr/travistestapp/MainActivity.kt
+++ app/src/main/java/de/sgr/travistestapp/MainActivity.kt
@@ -2,11 +2,13 @@ package de.sgr.travistestapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TEST", "We have to change something to trigger a travis build? ")
     }
 }
